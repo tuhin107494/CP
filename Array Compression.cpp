5dc112ad22f861fd398c909a71f57af6a92a3699
Tuhin Ahmed <67825154+tuhin107494@users.noreply.github.com>
2023-01-26 02:38:14 +0600

    Update Array Compression.cpp

--- Array Compression.cpp
+++ Array Compression.cpp
@@ -1,18 +1,21 @@
+const int N = 2e5+5;
+int L[N], R[N];
 void array_compression()
 {
     set<int> Set;
     for(int i=1; i<=n; i++)
     {
-        Set.insert(l[i]);
-        Set.insert(r[i]);
-        Set.insert(r[i]+1);
+        cin >> L[i] >> R[i];
+        Set.insert(L[i]);
+        Set.insert(R[i]);
+        Set.insert(R[i]+1);
     }
-    map<int, int> Map;
- 
-    for(auto u: Set) Map[u] = ++idx;
+    map<int, int> Map, key;
+    int idx = 0;
+    for(auto x: Set) Map[x] = ++idx;
     for(int i=1; i<=n; i++)
     {
-        l[i] = Map[l[i]];
-        r[i] = Map[r[i]];
+        L[i] = Map[L[i]];
+        R[i] = Map[R[i]];
     }
 }
